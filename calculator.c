21ceea53fd2bcee993d5c63a22a951c0ce9f694b
sup12123 <95585537+sup12123@users.noreply.github.com>
2022-07-26 16:53:13 +0000

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+int main () {
+
+int cock;
+
+int balls;
+
+printf("Type a number: \n");
+
+scanf("%d", &cock);
+
+printf("Type your second number: \n");
+
+scanf("%d", &balls);
+
+int sum = cock + balls;
+
+printf("%d", sum);
+
+return 0;
+}
+
+
+
+
